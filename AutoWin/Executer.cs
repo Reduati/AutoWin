f3eaba0bf9e8b51bdd410eb7d61fca8a5cf505db
fgsec <25889166+fgsec@users.noreply.github.com>
2020-08-12 11:38:40 -0300

    Update Executer.cs

--- AutoWin/Executer.cs
+++ AutoWin/Executer.cs
@@ -9,34 +9,35 @@ namespace AutoWin {
     class Executer {
 
 		public static bool Start(string technique) {
-
-			var technique_path =  Program.project_path + technique+".exe";
-
-			if(File.Exists(technique_path)) {
-				var bytes = File.ReadAllBytes(technique_path);
-				Console.WriteLine("Executing: " + technique);
-				Inject(bytes);
-				Console.WriteLine(technique);
-			} else {
-				Console.WriteLine("Cand find technique binary for ("+ technique_path + ")" + technique);
-            }
 			
+			try {
+				var technique_path = Program.project_path + technique + ".exe";
+				if (File.Exists(technique_path)) {
+					var bytes = File.ReadAllBytes(technique_path);
+					Console.WriteLine("Executing: " + technique);
+					Inject(bytes);
+					Console.WriteLine(technique);
+				} else {
+					Console.WriteLine("Cand find technique binary for (" + technique_path + ")" + technique);
+				}
+			} catch (Exception ex) {
+				Console.WriteLine("Error during executer start: " + ex.Message);
+			}
 			
 			return false;
         }
 		public static bool Inject(byte[] bytes) {
 
 			try {
-
 				var assembly = System.Reflection.Assembly.Load(bytes);
-
 				foreach (var type in assembly.GetTypes()) {
-
 					object instance = Activator.CreateInstance(type);
 					object[] args = new object[] { new string[] { "" } };
 					try {
 						type.GetMethod("Main").Invoke(instance, args);
-					} catch { Console.WriteLine("error"); }
+					} catch (Exception ex) {
+						Console.WriteLine("Error during injection: " + ex.Message); 
+					}
 				}
 				return true;
 			} catch (Exception ex) {
