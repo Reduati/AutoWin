55ca89a8528680051521ab014f0bce9bb5d66ca1
fgsec <25889166+fgsec@users.noreply.github.com>
2020-08-14 12:51:34 -0300

    Update build.ps1

--- AutoWin/build.ps1
+++ AutoWin/build.ps1
@@ -22,7 +22,7 @@ $techniques = Get-Childitem –Path $techniques_folder
 ForEach ($technique in $techniques) {
     write-host "[!] Found: $technique"
     $binfolder = "$techniques_folder$technique\bin\Debug\$technique.exe"
-    $csfolder = "$techniques_folder$technique\Program.cs"
+    $csfolder = "$techniques_folder$technique\$technique.sln"
     if(Test-Path($binfolder)) {
         Copy-Item -Path $binfolder -Destination $lib_folder
         write-host "[*] $technique - OK"
@@ -30,9 +30,11 @@ ForEach ($technique in $techniques) {
         write-host "[!] Trying to compile technique ($technique)"
         if(Test-Path($csfolder)) {
             $binname = "$lib_folder$technique.exe"
-            C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /out:$binname $csfolder
-            if(test-path($lib_folder)) {
-                write-host "[!] Technique $technique compiled!"
+            $binlocalname = "$techniques_folder$technique\bin\Debug\$technique.exe"
+            C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe $csfolder
+            
+            if(test-path($binlocalname)) {
+                Copy-Item -Path $binlocalname -Destination $binname
             }
         } else {
             write-host "[!] Brow, are u kidding? Can't even find the source! ($technique)"
