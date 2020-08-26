bdd3c42a9af3972d74d74795e18abc37018116d2
fgsec <25889166+fgsec@users.noreply.github.com>
2020-08-26 17:08:04 -0300

    Added T1135

---
+++ Techniques/T1135/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Technique {
+	/*
+	 *  T1135 - Network Share Discovery
+	 */
+	public static Dictionary<string, string> EntryData { get; set; }
+	public static Dictionary<string, string> ExitData { get; set; }
+
+	public static void Main(string[] args) {
+	}
+}
+
